6f423d693030ccdf4f7f6546aff99d0a86020320
Justin Auger <justinauger@gmail.com>
2020-04-16 15:04:38 +0100

    tidy day6

--- day6/source.cpp
+++ day6/source.cpp
@@ -2,65 +2,72 @@
 
 using namespace std;
 
+// Data Structures
 map<string, string> par;
-map<string, int> depth;
+map<string, int> depth_cache;
 
-void compute_depth(string s){
-    if(depth[s] != -1) return; // we already know depth[s]
-    compute_depth(par[s]);
-    depth[s] = depth[par[s]] + 1;
+int get_depth(string s){
+    if(depth_cache.count(s) != 0) return depth_cache[s];
+
+    int d = get_depth(par[s]) + 1;
+    depth_cache[s] = d;
+    return d;
 }
 
-int main(){
-    int N;
-    cin >> N;
-    
+int main() {
+    // COM, (Center of Mass planet) is the root of the tree
+    // ie it has no parent, and is at depth 0.
     par["COM"] = "N/A";
-    depth["COM"] = 0;
+    depth_cache["COM"] = 0;
 
-    for(int i=0;i<N;i++){
+    // INPUT PARSING - construct tree
+    int N; cin >> N;
+    for(int i=0;i<N;i++) {
         string s; cin >> s;
         int split = s.find(')');
 
+        // A ) B
+        // planet B orbits planet A
+        // we say A is parent of B
         string A = s.substr(0, split);
         string B = s.substr(split+1);
-
-        // cout << A << " " << B << endl;
-        // then A is parent of B
         par[B] = A;
-        depth[B] = -1;
     }
 
+    // PART ONE - find the depth of every vertex in the tree rooted at "COM"
     int total = 0;
-    for(auto [a, b] : par){
-        compute_depth(a);
-        total += depth[a];
-        cout << a << ":" << depth[a] << endl;
+    for(auto [a, a_parent] : par) {
+        total += get_depth(a);
     }
-    cout << total << endl;
+    cout << "Total direct/indirect orbits: " << total << endl;
 
+    // PART TWO - find the distance to the first common ancestor of "YOU" and "SAN"
     int transfers = 0;
     string iterA = "YOU";
-    int depthA = depth[iterA];
     string iterB = "SAN";
-    int depthB = depth[iterB];
+    int depthA = depth_cache[iterA];
+    int depthB = depth_cache[iterB];
 
-    // swap so B is at least as deep as A
+    // swap if necessary to achieve invariant (depthA <= depthB)
     if(!(depthA <= depthB)){
         swap(depthA, depthB);
         swap(iterA, iterB);
-    } 
-    for(int k=0;k<depthB-depthA;k++){
+    }
+    // travel up the chain (towards the root) from B, until we are at matching depth
+    while(depthA != depthB) {
         iterB = par[iterB];
+        depthB--;
         transfers++;
     }
-    // now A and B at the same level
-    while(iterA != iterB){
+    // now A and B at the same depth, travel towards the root in both
+    while (iterA != iterB) {
         iterA = par[iterA];
         iterB = par[iterB];
         transfers += 2;
     }
-    cout << transfers-2 << endl;
+
+    // Since we actually want the distance between par["YOU"] and par["SAN"], we subtract 2
+    cout << "Number of transfers: " << transfers-2 << endl;
 
     return 0;
 }
